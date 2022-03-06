97c9d5971f513e5251da9abfac87d647a636e7b0
xkz1899 <89337365+xkz1899@users.noreply.github.com>
2022-03-06 15:54:18 +0300

    Delete gulp directory

--- gulp/tasks/copy.js
+++
@@ -1,4 +0,0 @@
-export const copy = () => {
-  return app.gulp.src(app.path.src.favicon)
-    .pipe(app.gulp.dest(app.path.build.favicon));
-};
--- gulp/tasks/server.js
+++
@@ -1,9 +0,0 @@
-export const server = (done) => {
-    app.plugins.browserSync.init({
-    server: {
-        baseDir: `${app.path.build.html}`
-    },
-    notify: false, //Сообщения в браузере
-    port: 3000,
-    });
-}
